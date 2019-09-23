0adcb84fa16150ec6ebc42a6f178c28dc9708da1
Paul Page <pjpage98@gmail.com>
2019-09-22 19:54:26 -0500

    Cleanup, add multiple buffers, status bar, start work on cursor positioning

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,13 @@ extern crate sdl2;
 // use std::path::PathBuf;
 use std::fs;
 use std::io::{BufReader, BufRead};
+use std::env;
 
 use sdl2::event::Event;
 use sdl2::keyboard::{Keycode, Mod};
 use sdl2::rect::Rect;
 use sdl2::render::TextureQuery;
 use sdl2::pixels::Color;
-
-// static SCREEN_WIDTH : u32 = 800;
-// static SCREEN_HEIGHT : u32 = 600;
 
 // handle the annoying Rect i32
 macro_rules! rect(
@@ -20,27 +18,18 @@ macro_rules! rect(
     )
 );
 
-// fn bounded_dec(target: usize) -> usize {
-//     if target > 0 { target - 1 } else { target }
-// }
+struct Buffer {
+    contents: Vec<String>,
+    name: String,
+    is_dirty: bool,
+}
 
-// fn bounded_inc(target: usize, upper: usize) -> usize {
-//     if target < upper { target + 1 } else { target }
-// }
-
-// fn read_dir<T: Into<PathBuf>>(path: T) -> Vec<String> {
-//     fs::read_dir(&path.into())
-//         .unwrap()
-//         .map(|result| result.map(|entry| entry.path().display().to_string()).unwrap())
-//         .map(|s| s.replacen("./", "", 1))
-//         .collect()
+// impl Buffer {
 // }
 
 struct App {
-    width: u32,
-    height: u32,
-    line_height: u32,
-    buffer: Vec<String>,
+    line_height: i32,
+    buffers: Vec<Buffer>,
     x: usize,
     y: usize,
     scroll_idx: usize,
@@ -49,17 +38,15 @@ struct App {
 
     // distance from top of document to top of viewport in pixels. Allows smooth scrolling.
     scroll_offset: i32,
+
+    buffer_idx: usize,
 }
 
-// impl App {
-//     fn set_dir<T: std::convert::AsRef<std::path::Path>>(&mut self, path: T) {
-//         std::env::set_current_dir(path).unwrap();
-//         self.contents = read_dir(".");
-//         self.scroll_idx = 0;
-//         self.selected_idx = 0;
-//         self.search.clear();
-//     }
-// }
+impl App {
+    fn active_buffer(&mut self) -> &mut Buffer {
+        &mut self.buffers[self.buffer_idx]
+    }
+}
 
 fn main() -> Result<(), String> {
 
@@ -76,12 +63,11 @@ fn main() -> Result<(), String> {
     let texture_creator = canvas.texture_creator();
 
     // Initialize app
-    let (width, height) = canvas.window().size();
+    // let (width, height) = canvas.window().size();
     let mut app = App {
-        width: width,
-        height: height,
-        line_height: 16, //TODO
-        buffer: Vec::new(),
+        line_height: 16,
+        buffers: Vec::new(),
+        buffer_idx: 0,
         x: 0,
         y: 0,
         scroll_idx: 0,
@@ -89,33 +75,70 @@ fn main() -> Result<(), String> {
         font_size: 16,
         scroll_offset: 0,
     };
-    let font = ttf_context.load_font(app.font_path, app.font_size).unwrap();
 
-    // app.buffer.push(String::new());
-    let file = fs::File::open("src/main.rs").unwrap();
-    let reader = BufReader::new(file);
-    for line in reader.lines() {
-        app.buffer.push(line.unwrap());
+    let args: Vec<String> = env::args().collect();
+    match args.len() {
+        2 => {
+            app.buffers.push(Buffer {
+                contents: Vec::new(),
+                name: args[1].clone(),
+                is_dirty: false,
+            });
+            let file = fs::File::open(&args[1]).unwrap();
+            let reader = BufReader::new(file);
+            for line in reader.lines() {
+                app.buffers[app.buffer_idx].contents.push(line.unwrap());
+            }
+        },
+        _ => {
+            app.buffers.push(Buffer {
+                contents: Vec::new(),
+                name: "UNNAMED".to_string(),
+                is_dirty: false,
+            })
+        }
     }
+    // app.buffers.push(Buffer {
+    //     contents: Vec::new(),
+    // });
+    let font = ttf_context.load_font(&app.font_path, app.font_size).unwrap();
+    app.line_height = font.height();
+
+    // app.buffers[app.buffer_idx].contents.push(String::new());
 
     'mainloop: loop {
         for event in sdl_context.event_pump()?.poll_iter() {
             match event {
                 Event::Quit {..} => break 'mainloop,
                 Event::TextInput { text, .. } => {
-                    app.buffer[app.y].insert_str(app.x, &text);
+                    app.buffers[app.buffer_idx].contents[app.y].insert_str(app.x, &text);
                     app.x += 1;
+                    app.buffers[app.buffer_idx].is_dirty = true;
+                },
+                Event::MouseButtonDown { x, y, .. } => {
+                    // let current_line = app.buffers[app.buffer_idx].contents[app.y];
+                    let mut screen_x = 0;
+                    let mut char_x = 0;
+                    while char_x < x && (screen_x as usize) < app.buffers[app.buffer_idx].contents[app.y].len() {
+                        let (cx, _) = font.size_of(&app.buffers[app.buffer_idx].contents[app.y][..screen_x]).unwrap();
+                        char_x = cx as i32;
+                        screen_x += 1;
+                    }
+                    // TODO fix the sizing situation (padding, bars, etc.)
+                    let screen_y = ((y as f64 - app.line_height as f64 - 15 as f64) / app.line_height as f64).floor() as usize;
+                    app.x = screen_x;
+                    app.y = app.scroll_idx + screen_y;
                 },
                 Event::MouseWheel { y, .. } => {
                     let candidate = app.scroll_idx as i32 - (y * 3);
                     if candidate < 0 {
                         app.scroll_idx = 0;
-                    } else if candidate > app.buffer.len() as i32 {
-                        app.scroll_idx = app.buffer.len();
+                    } else if candidate > app.buffers[app.buffer_idx].contents.len() as i32 {
+                        app.scroll_idx = app.buffers[app.buffer_idx].contents.len();
                     } else {
                         app.scroll_idx = candidate as usize;
                     }
-                }
+                },
                 Event::KeyDown { keycode: Some(kc), keymod, .. } => {
                     match kc {
                         Keycode::Up => {
@@ -140,9 +163,9 @@ fn main() -> Result<(), String> {
                             }
                         },
                         Keycode::PageDown => {
-                            if app.scroll_idx > app.buffer.len() - 3 {
-                                app.scroll_idx = app.buffer.len();
-                                // app.scroll_offset = (app.buffer.len() * app.font_size as usize) as u32;
+                            if app.scroll_idx > app.buffers[app.buffer_idx].contents.len() - 3 {
+                                app.scroll_idx = app.buffers[app.buffer_idx].contents.len();
+                                // app.scroll_offset = (app.buffers[app.buffer_idx].contents.len() * app.font_size as usize) as u32;
                             } else {
                                 app.scroll_idx += 3;
                                 // app.scroll_offset += (3 * app.font_size as u32) as u32;
@@ -150,26 +173,13 @@ fn main() -> Result<(), String> {
                         },
                         Keycode::Backspace => {
                             if app.x > 0 {
-                                if app.x < app.buffer[app.y].len() {
-                                    app.buffer[app.y].remove(app.x - 1);
+                                if app.x < app.buffers[app.buffer_idx].contents[app.y].len() {
+                                    app.buffers[app.buffer_idx].contents[app.y].remove(app.x - 1);
                                 }
                                 app.x -= 1;
+                                app.buffers[app.buffer_idx].is_dirty = true;
                             }
                         }
-                        // Keycode::Escape => {
-                        //     app.search.clear();
-                        // },
-                        // Keycode::Backspace => {
-                        //     app.set_dir("..");
-                        // },
-                        // Keycode::Return => {
-                        //     let path =  &app.contents[app.selected_idx].clone();
-                        //     if fs::metadata(path).unwrap().is_dir() {
-                        //         app.set_dir(path);
-                        //     } else {
-                        //         println!("TODO: open file");
-                        //     }
-                        // },
                         _ => {
                             if keymod.contains(Mod::RCTRLMOD) || keymod.contains(Mod::LCTRLMOD) {
                                 match kc {
@@ -181,23 +191,10 @@ fn main() -> Result<(), String> {
                                     },
                                     Keycode::O => {
                                         println!("TODO: Open file");
-                                    }
+                                    },
+                                    _ => {},
                                 }
                             }
-                            // let c = kc.name();
-                            // if c.len() == 1 {
-                            //     app.buffer[app.y].insert_str(app.x, &c);
-                            //     app.x += 1;
-                            //     // TODO: Handle shifted keys
-                            //     // app.search.push_str(&c.to_lowercase());
-                            //     // for (i, entry) in app.contents[app.selected_idx..].iter().enumerate() {
-                            //     //     if entry.to_lowercase().starts_with(&app.search) {
-                            //     //         println!("{} == {}", entry.to_lowercase(), &app.search);
-                            //     //         app.selected_idx =  app.selected_idx + i;
-                            //     //         break;
-                            //     //     }
-                            //     // }
-                            // }
                         }
                     }
                 },
@@ -206,7 +203,7 @@ fn main() -> Result<(), String> {
         }
 
         // Smooth scrolling
-        let target_scroll_offset = app.scroll_idx as i32 * app.font_size as i32;
+        let target_scroll_offset = app.scroll_idx as i32 * app.line_height as i32;
         if app.scroll_offset < target_scroll_offset {
             app.scroll_offset += ((target_scroll_offset - app.scroll_offset) as f64 / 3.0).ceil() as i32;
         } else if app.scroll_offset > target_scroll_offset {
@@ -217,9 +214,7 @@ fn main() -> Result<(), String> {
         canvas.clear();
 
         // Draw the contents of the file and the cursor.
-        let mut total_height: i32 = 0 - app.scroll_offset as i32;
-        // for (i, entry) in app.buffer[app.scroll_idx..].iter().enumerate() {
-        for (i, entry) in app.buffer.iter().enumerate() {
+        for (i, entry) in app.buffers[app.buffer_idx].contents.iter().enumerate() {
 
             // Right-pad the string to allow the cursor to be rendered off the end of the line of
             // text
@@ -240,29 +235,38 @@ fn main() -> Result<(), String> {
             let surface = font.render(&render_text).blended(Color::RGBA(40, 0, 0, 255)).unwrap();
             let texture = texture_creator.create_texture_from_surface(&surface).unwrap();
             let TextureQuery { width, height, .. } = texture.query();
-            let target = rect!(0 + padding, total_height + padding as i32, width, height);
+            let target = rect!(0 + padding, i as i32 * app.line_height - app.scroll_offset + padding as i32 * 3 + app.line_height as i32, width, height);
             canvas.copy(&texture, None, Some(target))?;
 
             // Draw the cursor if we're rendering the cursor line
-            // if app.scroll_idx + i == app.y {
             if i == app.y {
                 // If the cursor isn't at the beginning of the line, render the text before the
                 // cursor so we can measure its width.
                 let text_right = &render_text[..app.x];
                 let mut cursor_x = 0;
-                if text_right.len() > 0 {
-                    let surface = font.render(&text_right).blended(Color::RGBA(40, 0, 0, 255)).unwrap();
-                    let texture = texture_creator.create_texture_from_surface(&surface).unwrap();
-                    let info = texture.query();
-                    cursor_x = info.width;
-                }
-                let cursor = rect!(cursor_x + padding, total_height + padding as i32, 3, height);
+                let (cursor_x, _) = font.size_of(text_right).unwrap();
+                let cursor = rect!(cursor_x + padding, i as i32 * app.line_height - app.scroll_offset + padding as i32 * 3 + app.line_height as i32, 3, height);
                 canvas.set_draw_color(Color::RGBA(0, 0, 0, 255));
                 canvas.fill_rect(cursor)?;
             }
-
-            total_height += height as i32;
         }
+
+        // Draw bar
+        let (width, _height) = canvas.window().size();
+        let padding = 5;
+        let rect = rect!(0, 0, width, app.line_height + padding * 2);
+        canvas.set_draw_color(Color::RGBA(50, 50, 50, 255));
+        canvas.fill_rect(rect)?;
+        let mut dirty_text = "";
+        if app.buffers[app.buffer_idx].is_dirty {
+            dirty_text = "*";
+        }
+        let bar_text = format!("{} {}", dirty_text, &app.buffers[app.buffer_idx].name);
+        let surface = font.render(&bar_text).blended(Color::RGBA(200, 200, 200, 255)).unwrap();
+        let texture = texture_creator.create_texture_from_surface(&surface).unwrap();
+        let TextureQuery { width: w, height: h, .. } = texture.query();
+        let target = rect!(0 + padding, 0 + padding, w, h);
+        canvas.copy(&texture, None, Some(target))?;
 
         canvas.present()
     }
