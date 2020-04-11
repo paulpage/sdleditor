fa356bcfd94e3c64d77a3e71507486aecb0d16b0
Paul Page <pjpage98@gmail.com>
2020-04-10 22:03:51 -0500

    Rudimentary syntax highlighting

--- src/pane.rs
+++ src/pane.rs
@@ -6,9 +6,32 @@ use std::cmp::{max, min};
 use std::collections::HashMap;
 use std::rc::Rc;
 
+use regex::Regex;
+
 use unicode_segmentation::UnicodeSegmentation;
 
 use crate::buffer::Buffer;
+
+struct ColorScheme {
+    fg: Color,
+    bg: Color,
+    ui_fg: Color,
+    ui_bg: Color,
+    ui_inactive_fg: Color,
+    ui_inactive_bg: Color,
+    selection: Color,
+    comment: Color,
+}
+
+struct Syntax {
+    line_comment: Regex,
+    block_comment_start: Regex,
+    block_comment_end: Regex,
+    keywords: Vec<String>,
+    string_start: Regex,
+    string_end: Regex,
+    has_nested_comments: bool,
+}
 
 pub enum PaneType {
     Buffer,
@@ -43,10 +66,33 @@ pub struct Pane<'a> {
     char_width: i32,
     pub font: Font<'a, 'static>,
     font_cache: HashMap<FontCacheKey, Rc<FontCacheEntry>>,
+    syntax: Syntax,
+    colors: ColorScheme,
 }
 
 impl<'a> Pane<'a> {
     pub fn new(font: Font<'a, 'static>, pane_type: PaneType, buffer_id: usize) -> Self {
+        // TODO Very incomplete
+        let syntax = Syntax {
+            line_comment: Regex::new(r"//").unwrap(),
+            block_comment_start: Regex::new(r"/\*").unwrap(),
+            block_comment_end: Regex::new(r"\*/").unwrap(),
+            string_start: Regex::new(r#"""#).unwrap(),
+            string_end: Regex::new(r#"""#).unwrap(),
+            keywords: vec!["fn".into(), "let".into()],
+            has_nested_comments: true,
+        };
+        let colors = ColorScheme {
+            fg: Color::RGB(253, 244, 193),
+            bg: Color::RGB(40, 40, 40),
+            ui_fg: Color::RGB(253, 244, 193),
+            ui_bg: Color::RGB(80, 73, 69),
+            ui_inactive_fg: Color::RGB(189, 174, 147),
+            ui_inactive_bg: Color::RGB(60, 56, 54),
+            selection: Color::RGB(168, 153, 132),
+            comment: Color::RGB(168, 153, 132), // TODO same as selection
+        };
+
         Pane {
             pane_type,
             x: 0,
@@ -61,21 +107,14 @@ impl<'a> Pane<'a> {
             char_width: font.size_of_char('o').unwrap().0 as i32,
             font,
             font_cache: HashMap::new(),
+            syntax,
+            colors,
         }
     }
 
     pub fn draw(&mut self, mut canvas: &mut WindowCanvas, buffer: &Buffer, is_active: bool) {
-        // Background
-        let color_bg = Color::RGB(40, 40, 40);
-        let color_fg = Color::RGB(253, 244, 193);
-        let color_selection1 = Color::RGB(168, 153, 132);
-        let (color_bar_bg, color_bar_fg) = if is_active {
-            (Color::RGB(80, 73, 69), Color::RGB(253, 244, 193))
-        } else {
-            (Color::RGB(60, 56, 54), Color::RGB(189, 174, 147))
-        };
         let padding = 5;
-        canvas.set_draw_color(color_bar_bg);
+        canvas.set_draw_color(self.colors.ui_bg);
         let rect = Rect::new(
             self.x - padding,
             self.y - padding,
@@ -83,7 +122,7 @@ impl<'a> Pane<'a> {
             self.h + padding as u32 * 2,
         );
         canvas.fill_rect(rect).unwrap();
-        canvas.set_draw_color(color_bg);
+        canvas.set_draw_color(self.colors.bg);
         let rect = Rect::new(self.x, self.y, self.w, self.h);
         canvas.fill_rect(rect).unwrap();
 
@@ -99,10 +138,30 @@ impl<'a> Pane<'a> {
 
         let bar_height: i32 = self.line_height as i32 + padding * 2;
 
+        let mut color = self.colors.fg;
+        let mut comment_level = 0;
+
         let chars_per_line = max(1, (self.w - padding as u32 * 4) / self.char_width as u32);
         let mut y = 0;
         let (sel_start_x, sel_start_y, sel_end_x, sel_end_y) = buffer.get_selection();
         for (i, line) in buffer.contents.iter().enumerate() {
+
+            let has_line_comment = self.syntax.line_comment.is_match(line);
+            let has_block_comment_start = self.syntax.block_comment_start.is_match(line);
+            let has_block_comment_end = self.syntax.block_comment_end.is_match(line);
+            let has_line_comment = self.syntax.line_comment.is_match(line);
+            let block_comment_start = if has_block_comment_start {
+                self.syntax.block_comment_start.find_iter(line).collect::<Vec<_>>()
+            } else {
+                vec![]
+            };
+            let block_comment_end = if has_block_comment_end {
+                self.syntax.block_comment_end.find_iter(line).collect::<Vec<_>>()
+            } else {
+                vec![]
+            };
+            let mut is_line_comment = false;
+
             if y < self.scroll_offset + self.h as i32 {
                 let mut unicode_line = line.as_str().graphemes(true).collect::<Vec<&str>>();
                 // Needed to draw cursor even if we're on a blank line
@@ -111,6 +170,39 @@ impl<'a> Pane<'a> {
                 }
                 let mut x = 0;
                 for (j, c) in unicode_line.iter().enumerate() {
+
+                    for m in &block_comment_start {
+                        if j == m.start() {
+                            comment_level += 1;
+                        }
+                    }
+                    for m in &block_comment_end {
+                        if j == m.end() {
+                            comment_level -= 1;
+                        }
+                    }
+                    if comment_level > 0 {
+                        color = self.colors.comment;
+                    } else {
+                        color = self.colors.fg;
+                        if let Some(m) = self.syntax.line_comment.find(line) {
+                            let mut block_overlaps = false;
+                            for m in &block_comment_end {
+                                if j == m.end() {
+                                    block_overlaps = true;
+                                }
+                            }
+                            if j == m.start() {
+                                if !has_block_comment_end || !block_overlaps {
+                                    is_line_comment = true;
+                                }
+                            }
+                        }
+                    }
+                    if is_line_comment {
+                        color = self.colors.comment;
+                    }
+
                     // Draw selection
                     if i >= sel_start_y && i <= sel_end_y {
                         if (j >= sel_start_x || i > sel_start_y) && (j < sel_end_x || i < sel_end_y)
@@ -121,7 +213,7 @@ impl<'a> Pane<'a> {
                                 self.char_width as u32,
                                 self.line_height as u32,
                             );
-                            self.fill_rect(&mut canvas, color_selection1, rect);
+                            self.fill_rect(&mut canvas, self.colors.selection, rect);
                         }
                     }
 
@@ -130,7 +222,7 @@ impl<'a> Pane<'a> {
                         if !c.trim().is_empty() {
                             self.draw_char(
                                 canvas,
-                                color_fg,
+                                color,
                                 (x * self.char_width as u32 + padding as u32 * 2) as i32,
                                 y - self.scroll_offset + padding * 2 + bar_height,
                                 c,
@@ -146,7 +238,7 @@ impl<'a> Pane<'a> {
                             2,
                             self.line_height as u32,
                         );
-                        self.fill_rect(&mut canvas, color_fg, rect);
+                        self.fill_rect(&mut canvas, self.colors.fg, rect);
                     }
 
                     x += 1;
@@ -161,17 +253,12 @@ impl<'a> Pane<'a> {
 
         // Draw the bar
         let rect = Rect::new(0, 0, self.w, bar_height as u32);
-        self.fill_rect(&mut canvas, color_bar_bg, rect);
+        self.fill_rect(&mut canvas, self.colors.ui_bg, rect);
         let dirty_text = if buffer.is_dirty { "*" } else { "" };
         let bar_text = vec![dirty_text, " ", &buffer.name];
-        self.draw_text(
-            &mut canvas,
-            color_bar_fg,
-            padding,
-            padding,
-            padding,
-            &bar_text[..],
-        );
+        for (i, c) in bar_text.iter().filter(|x| !x.is_empty()).enumerate() {
+            self.draw_char(canvas, self.colors.ui_fg, i as i32 * self.char_width + padding, padding, c);
+        }
     }
 
     pub fn handle_keystroke(&mut self, buffer: &mut Buffer, kstr: &str) -> bool {
@@ -277,53 +364,6 @@ impl<'a> Pane<'a> {
         // TODO We want to clamp this to the top and bottom of the buffer,
         // but it's not as easy as it used to be because of line wrapping
         self.scroll_lag += lines * self.line_height;
-    }
-
-    pub fn draw_text(
-        &mut self,
-        canvas: &mut WindowCanvas,
-        color: Color,
-        x: i32,
-        padding: i32,
-        y: i32,
-        text: &[&str],
-    ) -> (usize, i32) {
-        let mut length: i32 = 0;
-        if y > 0 && x > 0 {
-            for (i, c) in text.iter().filter(|x| !x.is_empty()).enumerate() {
-                let key = FontCacheKey {
-                    c: (*c).to_string(),
-                    color,
-                };
-                let tex = self.font_cache.get(&key).cloned().unwrap_or_else(|| {
-                    let surface = self.font.render(c).blended(color).unwrap();
-                    let texture = canvas
-                        .texture_creator()
-                        .create_texture_from_surface(&surface)
-                        .unwrap();
-                    let TextureQuery { width, height, .. } = texture.query();
-                    let resource = Rc::new(FontCacheEntry {
-                        texture,
-                        w: width,
-                        h: height,
-                    });
-                    self.font_cache.insert(key, resource.clone());
-                    resource
-                });
-
-                let texture = &tex.texture;
-                let w = min(self.w as i32 - (x + length) as i32, tex.w as i32) as u32;
-                let h = min(self.h as i32 - y as i32, tex.h as i32) as u32;
-                length += w as i32;
-                if length > self.w as i32 - padding {
-                    return (i, self.w as i32);
-                }
-                let source = Rect::new(0, 0, w, h);
-                let target = Rect::new(self.x + x + length as i32, self.y + y, w, h);
-                canvas.copy(&texture, Some(source), Some(target)).unwrap();
-            }
-        }
-        (text.len(), length)
     }
 
     pub fn select_all(&mut self, buffer: &mut Buffer) {
